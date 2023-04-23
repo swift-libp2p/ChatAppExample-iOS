d9acdf02a7bf1ddf65a75966e8e1104d7a5c4af4
Brandon <32753167+btoms20@users.noreply.github.com>
2023-04-23 11:15:25 -0700

    Move LibP2PService instantiation onto a background thread to prevent QOS thread inversion warnings. Formatted. Added comments.

--- LibP2PChatExample/Models/ViewModel.swift
+++ LibP2PChatExample/Models/ViewModel.swift
@@ -8,71 +8,73 @@
 import LibP2P
 import SwiftUI
 
-class ViewModel:ObservableObject, ChatDelegate {
-    @Published var groups:[String]
-    @Published var chats:[Chat]
-    
-    @Published var nickname:String? = nil {
+class ViewModel: ObservableObject, ChatDelegate {
+    @Published var groups: [String]
+    @Published var chats: [Chat]
+
+    @Published var nickname: String? = nil {
         didSet {
             /// Save the nickname in user defaults
-            UserDefaults.standard.set(nickname, forKey: "Nickname")
+            UserDefaults.standard.set(self.nickname, forKey: "Nickname")
         }
     }
-    
-    public var p2pService:LibP2PService
-    
-//    public var activeChats:[Chat] {
-//        self.chats.filter { $0.peer.isActive }
-//    }
-//    
-//    public var inactiveChats:[Chat] {
-//        self.chats.filter { !$0.peer.isActive }
-//    }
-    
+
+    public private(set) var p2pService: LibP2PService!
+
     init() {
-        // Grab a shared instance of our LibP2PService
-        self.p2pService = LibP2PService.shared
-        
         // Dummy data
         self.groups = ["No Groups Yet"]
         self.chats = []
-        
-        self.p2pService.topology(
-            TopologyRegistration(
-                protocol: "/chat/1.0.0",
-                handler: TopologyHandler(
-                    onConnect: onNewChatBuddy,
-                    onDisconnect: onChatBuddyLeft
-                )
-            )
-        )
-        
-        /// Register ourselves as the ChatDelegate
-        self.p2pService.delegate = self
-        
-        /// Restore the chats if possible...
+
+        // Restore the chats if possible...
         print("Attempting to restore chats")
         self.restoreChats()
-        
+
+        // Restore our Nickname if we have one saved
         if let nickname = UserDefaults.standard.string(forKey: "Nickname") {
             self.nickname = nickname
         }
-        
-        NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: .main) { _ in
-            print("Attempting to save chats")
-            self.saveChats()
-            self.p2pService.stop()
+
+        // Instantiate our LibP2PService on a background thread to prevent QOS inversion warnings
+        // - Note: there's a brief moment where our p2pService is nil, the forced unwrapping of it could cause a crash...
+        Task(priority: .background) {
+            // Grab a shared instance of our LibP2PService
+            self.p2pService = LibP2PService.shared
+
+            // Create a Topology subscription for the `/chat/1.0.0` protocol
+            // As libp2p discovers peers, and their supported protocols, it will notify us
+            self.p2pService.topology(
+                TopologyRegistration(
+                    protocol: "/chat/1.0.0",
+                    handler: TopologyHandler(
+                        onConnect: onChatBuddyJoined,
+                        onDisconnect: onChatBuddyLeft
+                    )
+                )
+            )
+
+            // Register ourselves as the ChatDelegate
+            // `p2pService` will call our `on(message:)` and `on(nickname:)` methods
+            self.p2pService.delegate = self
+
+            // Register to be notified when the user sends the app into the background so we can shut down libp2p and save our chats.
+            await NotificationCenter.default.addObserver(forName: UIApplication.willResignActiveNotification, object: nil, queue: .main) { _ in
+                print("App dismissed, saving chats and shutting down libp2p")
+                self.saveChats()
+                self.p2pService.stop()
+            }
         }
     }
-    
-    private func onNewChatBuddy(peer:PeerID, conn:Connection) {
+
+    /// This method gets called by our `Topology` Registration when a libp2p peer that supports the `/chat/1.0.0` protocol becomes active / comes online.
+    private func onChatBuddyJoined(peer: PeerID, conn: Connection) {
         DispatchQueue.main.async {
             guard !self.chats.contains(where: { $0.peer.peer == peer }) else {
                 // Mark the existing peer as active
                 if let index = self.chats.firstIndex(where: { $0.peer.peer == peer }) {
                     self.chats[index].peer.isActive = true
                 }
-                /// Let the existing peer know of our nickname if we have one set...
+                // Let the existing peer know of our nickname if we have one set...
                 DispatchQueue.global().async {
                     if let nickname = self.nickname {
                         self.send(nickname: nickname, to: peer)
@@ -80,7 +82,7 @@ class ViewModel:ObservableObject, ChatDelegate {
                 }
                 return
             }
-            
+
             print("New Chat Buddy")
             self.chats.append(
                 Chat(
@@ -93,17 +95,17 @@ class ViewModel:ObservableObject, ChatDelegate {
                     messages: []
                 )
             )
-            /// Let the new peer know of our nickname if we have one set...
+            // Let the new peer know of our nickname if we have one set...
             DispatchQueue.global().async {
                 if let nickname = self.nickname {
                     self.send(nickname: nickname, to: peer)
                 }
             }
         }
-        
     }
-    
-    private func onChatBuddyLeft(peer:PeerID) {
+
+    /// This method gets called by our `Topology` Registration when a libp2p peer that supports the `/chat/1.0.0` protocol disconnects / goes offline.
+    private func onChatBuddyLeft(peer: PeerID) {
         Task {
             DispatchQueue.main.async {
                 print("Chat Buddy Left")
@@ -113,8 +115,10 @@ class ViewModel:ObservableObject, ChatDelegate {
             }
         }
     }
-    
-    internal func on(message:String, from:PeerID) {
+
+    /// Part of our `ChatDelegate` protocol
+    /// This method is called (by our `LibP2PService`) everytime we receive a message from a peer using the `/chat/1.0.0` protocol
+    internal func on(message: String, from: PeerID) {
         DispatchQueue.main.async {
             print("We got a message from libP2P!")
             if let index = self.chats.firstIndex(where: { $0.peer.peer == from }) {
@@ -126,8 +130,10 @@ class ViewModel:ObservableObject, ChatDelegate {
             }
         }
     }
-    
-    internal func on(nickname:String, from:PeerID) {
+
+    /// Part of our `ChatDelegate` protocol
+    /// This method is called (by our `LibP2PService`) everytime we receive a nickname update from a peer using the `/chat/1.0.0` protocol
+    internal func on(nickname: String, from: PeerID) {
         DispatchQueue.main.async {
             print("We got a nickname from libP2P!")
             if let index = self.chats.firstIndex(where: { $0.peer.peer == from }) {
@@ -135,23 +141,32 @@ class ViewModel:ObservableObject, ChatDelegate {
             }
         }
     }
-    
+
+    /// Attempts to start the libp2p service
+    /// This includes...
+    /// - Starting a TCP Server and listening for inbound TCP requests
+    /// - Starting the mDNS Discovery service (searching for libp2p peers on the same network LAN)
     public func startP2PService() async {
         try? await self.p2pService.start()
     }
-    
+
+    /// Attempts to shutdown the libp2p service
+    /// This includes...
+    /// - Stopping the TCP Server
+    /// - Stopping the mDNS Discovery service
     public func stopP2PService() {
-        /// Stop the service
+        // Stop the service
         self.p2pService.stop()
-        /// Mark all peers inactive
+        // Mark all peers inactive
         for i in 0..<self.chats.count {
             self.chats[i].peer.isActive = false
         }
-        /// Save Chats
+        // Save Chats
         self.saveChats()
     }
-    
-    public func send(message:String, to chat:Chat) -> Message? {
+
+    /// Sends a single message to a Chat buddy
+    public func send(message: String, to chat: Chat) -> Message? {
         if let index = self.chats.firstIndex(where: { $0.id == chat.id }) {
             let msg = Message(message, type: .sent)
             self.chats[index].messages.append(msg)
@@ -160,20 +175,22 @@ class ViewModel:ObservableObject, ChatDelegate {
         }
         return nil
     }
-    
-    private func send(nickname:String, to peer:PeerID) {
+
+    /// Sends  a Nickname update message to a Chat buddy
+    private func send(nickname: String, to peer: PeerID) {
         self.p2pService.send(message: "nickname:\(nickname)", to: peer)
     }
-    
-    public func isActive(peer:Person) -> Bool {
+
+    /// Checks whether a Chat buddy is currently active (whether or not we have an open connection established with them)
+    public func isActive(peer: Person) -> Bool {
         if let index = self.chats.firstIndex(where: { $0.peer.peer == peer.peer }) {
             return self.chats[index].peer.isActive
         } else {
             return false
         }
     }
-    
-    /// Save the chats out to a file
+
+    /// Save the chats out to UserDefaults
     private func saveChats() {
         if let chatData = try? JSONEncoder().encode(self.chats) {
             UserDefaults.standard.set(chatData, forKey: "MyChats")
@@ -181,7 +198,7 @@ class ViewModel:ObservableObject, ChatDelegate {
         } else {
             print("Failed to save chats")
         }
-        
+
 //        let fileManager = FileManager.default
 //        do {
 //            let documentDirectory = try fileManager.url(for: ., in: .userDomainMask, appropriateFor:nil, create:false)
@@ -200,8 +217,8 @@ class ViewModel:ObservableObject, ChatDelegate {
 //            print(error)
 //        }
     }
-    
-    /// Restore the chats if a file exists
+
+    /// Restore the chats from UserDefaults if they exist
     private func restoreChats() {
         if let chatData = UserDefaults.standard.data(forKey: "MyChats") {
             if let chats = try? JSONDecoder().decode([Chat].self, from: chatData) {
@@ -211,7 +228,7 @@ class ViewModel:ObservableObject, ChatDelegate {
         } else {
             print("Failed to restore chats")
         }
-        
+
 //        let fileManager = FileManager.default
 //        do {
 //            let documentDirectory = try fileManager.url(for: .applicationDirectory, in: .userDomainMask, appropriateFor:nil, create:false)
@@ -227,7 +244,8 @@ class ViewModel:ObservableObject, ChatDelegate {
 //            print(error)
 //        }
     }
-    
+
+    /// Deletes all stored chats
     public func deleteChats() {
         // Delete all chats...
         self.chats = []
